e971e6b206d3eaf735a4259c481e6eb2321def75
Alabhya Jindal <alabhya10@gmail.com>
2024-04-16 21:37:19 +0530

    handleDragEnd

--- app/components/kanban/index.tsx
+++ app/components/kanban/index.tsx
@@ -2,7 +2,7 @@ import { useLoaderData } from '@remix-run/react'
 import { useEffect, useState } from 'react'
 import { loader } from '~/routes/_index'
 import Column from './column'
-import { DragDropContext } from '@hello-pangea/dnd'
+import { DragDropContext, DropResult } from '@hello-pangea/dnd'
 
 export interface TaskProps {
   id: number
@@ -23,8 +23,8 @@ export default function Kanban() {
   // console.log(data)
 
   const [data, setData] = useState()
-  const [done, setDone] = useState(null)
-  const [todo, setTodo] = useState(null)
+  const [todo, setTodo] = useState<TaskProps[]>([])
+  const [done, setDone] = useState<TaskProps[]>([])
 
   useEffect(() => {
     async function getData() {
@@ -38,14 +38,58 @@ export default function Kanban() {
     getData()
   }, [])
 
+  function handleDragEnd(result: DropResult) {
+    const { source, destination } = result
+    if (!destination) {
+      return
+    }
+
+    if (
+      destination.droppableId === source.droppableId &&
+      destination.index === source.index
+    ) {
+      return
+    }
+
+    const sourceColumn = source.droppableId === 'To do' ? todo : done
+    const destinationColumn = destination.droppableId === 'To do' ? todo : done
+
+    if (sourceColumn === destinationColumn) {
+      // Moving within the same list
+      const newTasks = Array.from(sourceColumn)
+      const [removed] = newTasks.splice(source.index, 1)
+      newTasks.splice(destination.index, 0, removed)
+
+      if (source.droppableId === 'To do') {
+        setTodo(newTasks)
+      } else {
+        setDone(newTasks)
+      }
+    } else {
+      // Moving from one list to another
+      const startTasks = Array.from(sourceColumn)
+      const finishTasks = Array.from(destinationColumn)
+      const [removed] = startTasks.splice(source.index, 1)
+
+      finishTasks.splice(destination.index, 0, {
+        ...removed,
+        completed: destination.droppableId === 'Done',
+      })
+
+      if (source.droppableId === 'To do') {
+        setTodo(startTasks)
+        setDone(finishTasks)
+      } else {
+        setDone(startTasks)
+        setTodo(finishTasks)
+      }
+    }
+  }
+
   return (
     <div className='bg-gray-200 flex-1'>
       <section className='mt-24 mx-4 flex justify-around'>
-        <DragDropContext
-          onDragEnd={() => {
-            console.log('drag end')
-          }}
-        >
+        <DragDropContext onDragEnd={handleDragEnd}>
           <Column title='To do' tasks={todo} />
           <Column title='Done' tasks={done} />
         </DragDropContext>
